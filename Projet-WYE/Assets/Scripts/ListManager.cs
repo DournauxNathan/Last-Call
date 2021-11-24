05dc058cd3bebea39fdf4d4168c10a08edd01b81
DournauxNathan <n.dournaux@rns.ecolescreatives.com>
2021-11-24 14:46:00 +0100

    Spotlight scene
    Create a new scene to test models with differents lightnings setup.
    
    Co-Authored-By: Maxime Rousseau <36676003+MaxmeRousseau@users.noreply.github.com>

--- Projet-WYE/Assets/Scripts/ListManager.cs
+++ Projet-WYE/Assets/Scripts/ListManager.cs
@@ -26,20 +26,36 @@ public class ListManager : MonoBehaviour
 
         if (LockedInteractors.Count == 0 && HoveredInteractors.Count>0) //Fonctionne ! -> ajoute le premier objet si liste vide != null
         {
-            LockedInteractors.Add(HoveredInteractors[0]);    
+            LockedInteractors.Add(HoveredInteractors[0]);
+            HoveredInteractors[0].GetComponent<ObjectManager>().Locked();
         }
 
         if(LockedInteractors.Count!=0 && HoveredInteractors.Count>0 && !LockedInteractors.Contains(HoveredInteractors[0]))
         {
             LockedInteractors.Add(HoveredInteractors[0]);
+            HoveredInteractors[0].GetComponent<ObjectManager>().Locked();
         }
 
         if(LockedInteractors.Count == 2)
         {
             CheckCompatibility(LockedInteractors[0], LockedInteractors[1]);
+            for (int i = 0; i < LockedInteractors.Count; i++)
+            {
+                LockedInteractors[i].GetComponent<ObjectManager>().UnLocked();
+            }
             LockedInteractors.Clear();
         }
     }
+
+    public void PressToCancel()
+    {
+        for (int i = 0; i < LockedInteractors.Count; i++)
+        {
+            LockedInteractors[i].GetComponent<ObjectManager>().UnLocked();
+        }
+        LockedInteractors.Clear();
+    }
+
 
     public void CheckCompatibility(GameObject objet1,GameObject objet2)
     {
@@ -71,5 +87,10 @@ public class ListManager : MonoBehaviour
         {
             Debug.Log("No one has combinable");
         }
+    }
+
+    public void DebugController()
+    {
+        Debug.Log("Click !");
     }
 }
--- Projet-WYE/Assets/Scripts/ObjectManager.cs
+++ Projet-WYE/Assets/Scripts/ObjectManager.cs
@@ -13,10 +13,15 @@ public class ObjectManager : MonoBehaviour
     public List<GameObject> subList;
     //private GameObject[] subList = new GameObject[1];
 
+    private Color baseColor;
+    public Color lockColor;
+    public bool isLocked = false;
+
 
     // Start is called before the first frame update
     void Start()
     {
+        baseColor = GetComponent<Outline>().OutlineColor;
         subList.Add(gameObject);
 
         if (outline == null)
@@ -53,6 +58,23 @@ public class ObjectManager : MonoBehaviour
 
     public void Disabled()
     {
-        outline.enabled = false;
+        if (!isLocked)
+        {
+            outline.enabled = false;
+        }
+        
+    }
+
+    public void Locked()
+    {
+        isLocked = true;
+        outline.OutlineColor = lockColor;
+    }
+
+    public void UnLocked()
+    {
+        isLocked = false;
+        outline.OutlineColor = baseColor;
+        Disabled();
     }
 }
