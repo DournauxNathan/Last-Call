2a8ca9b55e602a408cd02716ae5f757eb4635bff
DournauxNathan <n.dournaux@rns.ecolescreatives.com>
2021-12-28 09:16:05 +0100

    Dissolving Shader

---
+++ Projet-WYE/Assets/Scripts/Reveal.cs
@@ -0,0 +1,16 @@
+﻿//Shady
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Reveal : MonoBehaviour
+{
+    [SerializeField] Material Mat;
+    [SerializeField] Light SpotLight;
+	
+	void Update ()
+    {
+        Mat.SetVector("MyLightPosition",  SpotLight.transform.position);
+        Mat.SetVector("MyLightDirection", -SpotLight.transform.forward );
+        Mat.SetFloat ("MyLightAngle", SpotLight.spotAngle         );
+    }//Update() end
+}//class end
