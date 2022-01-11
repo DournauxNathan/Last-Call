2e1555af0d832a0e50e4c35e837170eb724b0d57
Maxime Rousseau <36676003+MaxmeRousseau@users.noreply.github.com>
2022-01-11 15:47:37 +0100

    Socket Selective

--- Projet-WYE/Assets/Scripts/XRSocketInteractorWithAutoSetup.cs
+++ Projet-WYE/Assets/Scripts/XRSocketInteractorWithAutoSetup.cs
@@ -5,9 +5,42 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class XRSocketInteractorWithAutoSetup : XRSocketInteractor
 {
+
+    [Header("")]
+    [SerializeField]
+    private bool isMergeable;
+    [SerializeField]
+    private GameObject mergewith;
+
+
     protected override void Awake()
     {
         base.Awake();
         interactionManager = MasterManager.Instance.xRInteractionManager;
     }
+
+
+    public override bool CanHover(XRBaseInteractable interactable)
+    {
+        if (isMergeable)
+        {
+            return base.CanSelect(interactable) && MatchUsingGameObject(interactable);
+        }
+        return base.CanSelect(interactable);
+    }
+
+    public override bool CanSelect(XRBaseInteractable interactable)
+    {
+        if (isMergeable)
+        {
+            return base.CanSelect(interactable) && MatchUsingGameObject(interactable);
+        }
+        return base.CanSelect(interactable);
+    }
+
+    private bool MatchUsingGameObject(XRBaseInteractable interactable)
+    {
+        return interactable.gameObject == mergewith;
+    }
+
 }
