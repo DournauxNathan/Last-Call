c2780c78d02ca88ded2aa197d4646303e1b634ce
DournauxNathan <n.dournaux@rns.ecolescreatives.com>
2022-02-16 20:49:01 +0100

    Resolve Combinable Changes

--- Projet-WYE/Assets/Scripts/CombinableObject.cs
+++ Projet-WYE/Assets/Scripts/CombinableObject.cs
@@ -5,79 +5,40 @@ using UnityEngine.Events;
 using UnityEngine.XR;
 using UnityEngine.XR.Interaction.Toolkit;
 
-[RequireComponent(typeof(Outline), typeof(SphereCollider))]
-public class CombinableObject : MonoBehaviour
+//[RequireComponent(typeof(Outline), typeof(SphereCollider))]
+public class CombinableObject : CombinableObject_Data
 {
-    public CombinableObject_Data objectData;
 
-    public ObjectData data;
-    public List<Combinaisons> combinaisons;
-    public List<GameObject> subList; //???? What is it ? Can't Remember ?
+    //public OrderFormat resultOrder;
 
-    public Outline outline;
-    public Material selectOutline;
-    public Color baseColor;
-    public Color selectColor;
-    
-    private ObjectActivator init;
+    [HideInInspector] public bool isLocked = false;
 
-    private void Awake()
-    {
-        SetTriggerCollider(true);
-        SetOutline();
-    }
-
+    //public List<Combinaisons> combinaisons;
+    //public List<GameObject> subList; //???? What is it ? Can't Remember ?
 
     // Start is called before the first frame update
     void Start()
     {
-        subList.Add(gameObject);   
+        //subList.Add(gameObject);   
 
         if (MasterManager.Instance.objectActivator != null)
         {
-            init = MasterManager.Instance.objectActivator;        
-
-            if(init.objectByIdList.ContainsKey(data.iD) )
+            if (MasterManager.Instance.objectActivator.objectByIdList.ContainsKey(iD))
             {
                 List<GameObject> tempObject;
 
-                tempObject = init.objectByIdList[data.iD];
-                subList.AddRange(tempObject);
-                init.objectByIdList.Remove(data.iD);
-                init.objectByIdList.Add(data.iD, subList);
+                tempObject = MasterManager.Instance.objectActivator.objectByIdList[iD];
+                //subList.AddRange(tempObject);
+                MasterManager.Instance.objectActivator.objectByIdList.Remove(iD);
+                //init.objectByIdList.Add(data.iD, subList);
                 return;
-            }
+            }/*
             else
             {
                 init.objectByIdList.Add(data.iD, subList);
-            }
+            }*/
         }
     }
-
-    private void SetOutline()
-    {
-        if (outline == null)
-        {
-            outline = GetComponent<Outline>();
-        }
-
-        outline.enabled = false;
-
-        baseColor = GetComponent<Outline>().OutlineColor;
-        selectColor = selectOutline.color;
-    }
-
-    private void SetTriggerCollider(bool newState)
-    {
-        if (GetComponents<SphereCollider>().Length == 2)
-        {
-            GetComponents<SphereCollider>()[1].isTrigger = newState;
-        }
-        else
-        {
-            GetComponent<SphereCollider>().isTrigger = newState;
-        }
-    }    
 
     private void OnTriggerEnter(Collider other)
     {
@@ -103,59 +64,70 @@ public class CombinableObject : MonoBehaviour
         }
     }
 
+    public void ToggleOutline()
+    {
+        if (!isLocked)
+        {
+            outline.enabled = false;
+        }
+        else
+        {
+            outline.enabled = true;
+        }
+    }
 
-    public void Enable()
+    public void Lock(bool b)
+    {
+        if (MasterManager.Instance.isInImaginary && b)
+        {
+            isLocked = true;
+
+            outline.OutlineColor = selectOutline.color;
+        }
+        else if (MasterManager.Instance.isInImaginary && !b)
+        {
+            isLocked = false;
+
+            outline.OutlineColor = defaultOutlineColor;
+
+            ToggleOutline();
+        }
+    }
+
+    //Old Code
+    /*public void EnableOutline()
     {
         outline.enabled = true;
     }
 
-    public void Disabled()
+    public void DisabledOutline()
     {
-        if (!data.isLocked)
+        if (!isLocked)
         {
             outline.enabled = false;
         }
     }
+    */
 
-    public void Locked()
-    {
-        if (MasterManager.Instance.isInImaginary)
-        {
-            data.isLocked = true;
-            outline.OutlineColor = selectColor;
-        }
-    }
+    /* public void Locked()
+     {
+         if (MasterManager.Instance.isInImaginary)
+         {
+             isLocked = true;
 
-    public void UnLocked()
-    {
-        if (MasterManager.Instance.isInImaginary)
-        {
-            data.isLocked = false;
-            outline.OutlineColor = baseColor;
-            Disabled();
-        }
-    }
-}
+             outline.OutlineColor = selectOutline.color;
+         }
+     }
 
-[Serializable]
-public class Combinaisons
-{
-    public GameObject combineWith;    
-}
+     public void UnLocked()
+     {
+         if (MasterManager.Instance.isInImaginary)
+         {
+             isLocked = false;
 
-[Serializable]
-public class ObjectData
-{
-    public int iD;
-    public bool isStatic;
-    public OrderFormat resultOrder;
+             outline.OutlineColor = defaultOutlineColor;
 
-    [HideInInspector] public bool isLocked = false;
-}
-
-public enum ObjectType
-{
-    None,
-    Useful,
-    Useless
+             ToggleOutline();
+         }
+     }*/
 }
