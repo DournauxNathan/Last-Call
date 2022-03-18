cc97d8fef924be10a333da069b3997b3f4354c07
DournauxNathan <n.dournaux@rns.ecolescreatives.com>
2022-03-18 10:35:51 +0100

    Projection Post Process

--- Projet-WYE/Assets/VFX/Script/DissolveEffect.cs
+++ Projet-WYE/Assets/VFX/Script/DissolveEffect.cs
@@ -5,14 +5,14 @@ using UnityEngine.VFX;
 using UnityEngine.InputSystem;
 using UnityEditor;
 
-public class DissolveEffect : MonoBehaviour
+public class DissolveEffect : Singleton<DissolveEffect>
 {
     [Header("Refs")]
     private GameObject particlePrefab;
     public ParticleSystem particles;
 
     [Header("Properties")]
-    public float dissolveRate = 0.02f;
+    public float dissolveRate = 0.8f;
     public float refreshRate = 0.05f;
     public float delay = 0.02f;
     public bool startEffect = false;
@@ -49,25 +49,18 @@ public class DissolveEffect : MonoBehaviour
         }
     }
 
-    IEnumerator Dissolve()
+    public IEnumerator Dissolve()
     {
-        if (particles != null)
-        {
-            particles.gameObject.SetActive(true);
-            particles.Play();
-        }
-        else
-        {
-            Debug.LogWarning("There is no particles, Object reference is missing in Inspector");
-        }
+        
 
-        float counter = 0;
+        float counter = 30;
 
         if (dissolveMaterials.Length > 0)
         {
-            while (dissolveMaterials[0].GetFloat("_Dissolve") < 1)
+            while (dissolveMaterials[0].GetFloat("_Dissolve") > 1)
             {
-                counter += dissolveRate;
+
+                counter -= Time.deltaTime * dissolveRate;
 
                 for (int i = 0; i < dissolveMaterials.Length; i++)
                 {
@@ -77,6 +70,33 @@ public class DissolveEffect : MonoBehaviour
                 yield return new WaitForSeconds(refreshRate);
             }
         }
+        else
+        {
+            Debug.LogWarning("List is empty");
+        }
+
+        if (particles != null)
+        {
+            particles.gameObject.SetActive(true);
+            particles.Play();
+
+            Debug.Log("hey");
+        }
+        else
+        {
+            Debug.LogWarning("There is no particles, Object reference is missing in Inspector");
+        }
+
+        if (dissolveMaterials[0].GetFloat("_Dissolve") <= 0)
+        {
+            for (int i = 0; i < dissolveMaterials.Length; i++)
+            {
+                dissolveMaterials[i].SetFloat("_Dissolve", 0f);
+                gameObject.SetActive(false);
+            }
+        }
+
+        //gameObject.SetActive(false);
     }
 }
 
