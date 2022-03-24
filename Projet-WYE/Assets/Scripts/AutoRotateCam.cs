4f071ded474a25872da4c10917951611e4333945
Maxime Rousseau <36676003+MaxmeRousseau@users.noreply.github.com>
2022-03-24 08:45:44 +0100

    fixed bug auto rotate + added in some scene

--- Projet-WYE/Assets/Scripts/AutoRotateCam.cs
+++ Projet-WYE/Assets/Scripts/AutoRotateCam.cs
@@ -12,21 +12,14 @@ public class AutoRotateCam : MonoBehaviour
     {
         xrRig = MasterManager.Instance.xrRig;
 
-
         if (xrRig != null)
         {
             xrRig.transform.LookAt(this.transform);
-
+            xrRig.transform.rotation = new Quaternion(0, xrRig.transform.rotation.y,0,xrRig.transform.rotation.w); 
         }
         else
         {
             Debug.LogError("No rig refenced");
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
