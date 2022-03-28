61c73597688ff03e3e2c8b99f2fe79988fa73d33
Maxime Rousseau <36676003+MaxmeRousseau@users.noreply.github.com>
2022-03-28 14:01:47 +0200

    TestNewEffect

---
+++ Projet-WYE/Assets/Scripts/SherlockEffect.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SherlockEffect : MonoBehaviour
+{
+    // Start is called before the first frame update
+   
+    public Transform cameraTransform;
+    public float time;
+    //Set it to whatever value you think is best
+    public float distanceFromCamera;
+
+    void Start()
+    {
+        cameraTransform = MasterManager.Instance.xrRig.transform.GetChild(0).GetChild(1);
+    }
+
+
+    void Update()
+    {
+        Vector3 resultingPosition = cameraTransform.position + cameraTransform.forward * distanceFromCamera;
+        transform.position = Vector3.Lerp(transform.position, resultingPosition, Time.deltaTime * time);
+        transform.rotation = cameraTransform.rotation;
+    }
+}
