eeffd8985d34af9667070a834c4411b3b2ab51b3
DournauxNathan <n.dournaux@rns.ecolescreatives.com>
2022-04-27 18:23:16 +0200

    Hammer Puzzle

---
+++ Projet-WYE/Assets/Scripts/Nails.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nails : MonoBehaviour
+{
+    public float offset;
+
+    public int count;
+    public Transform t1, t2;
+
+    public void DriveNail()
+    {
+        count++;
+
+        if (count == 1)
+        {
+            transform.position = t1.position;
+        }
+        if (count == 2)
+        {
+            count = 2;
+            transform.position = t2.position;
+        }
+    }
+}
--- Projet-WYE/Assets/Scripts/ThrowDetection.cs
+++ Projet-WYE/Assets/Scripts/ThrowDetection.cs
@@ -7,17 +7,12 @@ public class ThrowDetection : MonoBehaviour
     public string _tag;
     public bool useComparTag;
 
-
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log(other.CompareTag(_tag) && useComparTag && other.GetComponent<CombinableObject>() != null);
-
         if (other.CompareTag(_tag) && useComparTag && other.GetComponent<CombinableObject>() != null)
         {
-            if (OrderController.Instance.GetNumberOfPuzzleSucced() < 0 && OrderController.Instance.GetNumberOfPuzzleSucced() > 1)
-            {
-                OrderController.Instance.ResolvePuzzle();
-            }
+            OrderController.Instance.ResolvePuzzle();
+            useComparTag = false;
         }
     }
 }
