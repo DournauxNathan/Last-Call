d1123463745d62737da9671d470a3f55b78f8fc6
Maxime Rousseau <36676003+MaxmeRousseau@users.noreply.github.com>
2022-05-04 16:37:19 +0200

    Check Limits

--- Projet-WYE/Assets/Scripts/SherlockEffect.cs
+++ Projet-WYE/Assets/Scripts/SherlockEffect.cs
@@ -2,32 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SherlockEffect : MonoBehaviour
+public class SherlockEffect : Singleton<SherlockEffect>
 {
     public Transform cameraTransform;
     public Transform calculatedTransform;
     public float distanceFromCamera;
     public float time;
-    public List<Transform> words;
-    [SerializeField] private float minOffsetFloat;
-    [SerializeField] private float maxOffsetFloat;
+    public List<Vector2> offsets;
+
+    public OffsetLimit limit;
 
     private void Start() {
-        words = new List<Transform>();
+        limit = new OffsetLimit(-0.65f, 0.65f, -0.25f, 0.25f); //TODO: Change when testing in VR
     }
 
-    public void SetUp()
-    {
-        for (var i = 0; i < transform.childCount; i++)
-        {
-            words.Add(transform.GetChild(i));
-        }
-        AddOffSet(); 
-    }
-    public void ClearList()
-    {
-        words.Clear();
-    }
 
     void FixedUpdate()
     {
@@ -38,11 +26,34 @@ public class SherlockEffect : MonoBehaviour
         calculatedTransform = transform;
     }
 
-    void AddOffSet()
+    public void AddOffset(Transform _transform)
     {
-        for (var i = 0; i < words.Count; i++)
-        {
-            words[i].position += new Vector3(Random.Range(minOffsetFloat, maxOffsetFloat), Random.Range(minOffsetFloat, maxOffsetFloat), Random.Range(minOffsetFloat, maxOffsetFloat));
-        }        
+        offsets.Add(new Vector2(_transform.localPosition.x, _transform.localPosition.y));
     }
+
+    //Check if the offset is in the limit
+    public void CheckOffset(Transform _transform){
+        if(_transform.position.x> limit.maxX|| _transform.position.x< limit.minX|| _transform.position.y> limit.maxY|| _transform.position.y< limit.minY){
+            int index = _transform.GetSiblingIndex();
+            _transform.position = new Vector3(offsets[index].x, offsets[index].y, _transform.position.z);
+        }
+
+    }
+}
+
+//class OffsetLimit{
+public class OffsetLimit
+{
+    public float minX;
+    public float maxX;
+    public float minY;
+    public float maxY;
+
+    public OffsetLimit (float minX, float maxX, float minY, float maxY){ //Constructor
+        this.minX = minX;
+        this.maxX = maxX;
+        this.minY = minY;
+        this.maxY = maxY;
+    }
+
 }
