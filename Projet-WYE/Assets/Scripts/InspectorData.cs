298906ef25085b2fcfdcaf08faedfce812d72965
DournauxNathan <n.dournaux@rns.ecolescreatives.com>
2022-05-11 09:44:19 +0200

    Test Inspector Object

--- Projet-WYE/Assets/Scripts/InspectorData.cs
+++ Projet-WYE/Assets/Scripts/InspectorData.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 public class InspectorData : MonoBehaviour
@@ -8,6 +9,7 @@ public class InspectorData : MonoBehaviour
     private InspectionInWorld inspection;
     [Header("Data")]
     public List<string> _dataList;
+    public Image spriteSheet;
 
     [SerializeField] private bool testBool = false;
     [SerializeField] private bool hasGenerate = false;
