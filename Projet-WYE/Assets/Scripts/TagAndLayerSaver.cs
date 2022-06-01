44b555363c9db6d9f27f012568128f17de9ae33e
DournauxNathan <n.dournaux@rns.ecolescreatives.com>
2022-06-01 14:21:41 +0200

    Tag and Layer Load & Save

---
+++ Projet-WYE/Assets/Scripts/TagAndLayerSaver.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TagAndLayerSaver : MonoBehaviour
+{
+    public string myTag;
+    public int myLayer;
+
+}
