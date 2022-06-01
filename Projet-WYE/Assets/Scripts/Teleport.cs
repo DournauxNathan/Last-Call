724dcbe53899db6ef14e15cb1875fb29d5e35638
DournauxNathan <n.dournaux@rns.ecolescreatives.com>
2022-06-01 11:20:59 +0200

    Teleport de ses mort

--- Projet-WYE/Assets/Scripts/Teleport.cs
+++ Projet-WYE/Assets/Scripts/Teleport.cs
@@ -34,7 +34,7 @@ public class Teleport : Singleton<Teleport>
                 MasterManager.Instance.references.player.transform.position = position.position;
                 if (particle != null)
                 {
-                    particle.SetActive(false);
+                    //particle.SetActive(false);
                 }
             }
             else if (teleportAtStart)
@@ -42,7 +42,7 @@ public class Teleport : Singleton<Teleport>
                 MasterManager.Instance.references.player.transform.position = position.position;
                 if (particle != null)
                 {
-                    particle.SetActive(false);
+                    //particle.SetActive(false);
                 }
             }   
         }
@@ -73,14 +73,14 @@ public class Teleport : Singleton<Teleport>
         doAction?.Invoke();
 
         m_Collider.isTrigger = true;
-        particle.SetActive(false);
+        //particle.SetActive(false);
 
         if (GetComponentInChildren<Renderer>() != null)
         {
             GetComponentInChildren<Renderer>().enabled = false;
             if (particle != null)
             {
-                particle.SetActive(false);
+                //particle.SetActive(false);
             }
         }
     }
