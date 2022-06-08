9fe706ae4e47b5ef1cea153fdd129a5c0a3819f6
Duhommet-Jules-etpa <59608473+Duhommet-Jules-etpa@users.noreply.github.com>
2022-06-08 14:11:22 +0200

    Test Assiete

--- Projet-WYE/Assets/Materials/Broken_Object/CollisionObj.cs
+++ Projet-WYE/Assets/Materials/Broken_Object/CollisionObj.cs
@@ -7,28 +7,14 @@ public class CollisionObj : MonoBehaviour
     [SerializeField] private GameObject fracturedObject;
     public List<string> collisionTags;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log(other.gameObject.name);
-        foreach (var tag in collisionTags)
+
+        if (other.CompareTag(collisionTags[0]))
         {
-            if (other.tag == tag)
-            {
-                Instantiate(fracturedObject, transform.position, transform.rotation);
-                Destroy(gameObject);
-            }
+            Instantiate(fracturedObject, transform.position, transform.rotation);
+            Destroy(gameObject);
         }
     }
 }
